844a732395228e99a4573bbe5b0c84c1f5a9cb20
linjunjie <129569154+Snorlaxkaka@users.noreply.github.com>
2025-08-17 18:41:29 +0800

    update：文章编辑修改

--- src/views/ArticleEdit.vue
+++ src/views/ArticleEdit.vue
@@ -27,6 +27,25 @@
           <i v-else class="el-icon-plus avatar-uploader-icon"></i>
         </el-upload>
       </el-form-item>
+      <!-- 仅编辑时显示时间选择器 -->
+      <el-form-item label="创建时间" v-if="id">
+        <el-date-picker
+          v-model="model.createdAt"
+          type="datetime"
+          format="yyyy-MM-dd HH:mm:ss"
+          value-format="yyyy-MM-ddTHH:mm:ss"
+          placeholder="选择创建时间"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item label="更新时间" v-if="id">
+        <el-date-picker
+          v-model="model.updatedAt"
+          type="datetime"
+          format="yyyy-MM-dd HH:mm:ss"
+          value-format="yyyy-MM-ddTHH:mm:ss"
+          placeholder="选择更新时间"
+        ></el-date-picker>
+      </el-form-item>
       <el-form-item label="详情">
         <div>
           <mavon-editor
@@ -65,10 +84,17 @@ export default {
   },
   methods: {
     async save() {
+      // 新建时移除时间字段（防止意外提交）
+      const submitData = { ...this.model }
+      if (!this.id) {
+        delete submitData.createdAt
+        delete submitData.updatedAt
+      }
+
       if (this.id) {
-        await this.$http.put(`rest/articles/${this.id}`, this.model)
+        await this.$http.put(`rest/articles/${this.id}`, submitData)
       } else {
-        await this.$http.post('rest/articles', this.model)
+        await this.$http.post('rest/articles', submitData)
       }
       this.$router.push('/articles/list')
       this.$message({
@@ -84,13 +110,6 @@ export default {
       const res = await this.$http.get('rest/categories')
       this.categories = res.data
     },
-    // async handleImageAdded(file, Editor, cursorLocation, resetUploader) {
-    //   const formData = new FormData();
-    //   formData.append("file", file);
-    //   const res = await this.$http.post("upload", formData);
-    //   Editor.insertEmbed(cursorLocation, "image", res.data.url);
-    //   resetUploader();
-    // }
     $imgAdd(pos, $file) {
       const formData = new FormData()
       formData.append('file', $file)
