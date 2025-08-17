f0820b60ffa4c4711a3017e87869b547dc8fca12
linjunjie <129569154+Snorlaxkaka@users.noreply.github.com>
2025-08-17 18:28:31 +0800

    update：dev的后端访问路径

--- vue.config.js
+++ vue.config.js
@@ -14,24 +14,6 @@ module.exports = {
     port: 8088,
     // 关闭主机检查，避免局域网内其他设备无法访问
     disableHostCheck: true,
-    // 添加跨域代理配置
-    proxy: {
-      // 匹配所有以 "/admin/api" 开头的请求路径
-      '/admin/api': {
-        target: 'https://blogapi.chengfen.online',  // 后端API的基础地址
-        changeOrigin: true,  // 关键：模拟同源请求
-        ws: true,  // 支持WebSocket
-        secure: true,  // 如果是https接口，需要配置这个参数
-        // 确保请求头正确
-        headers: {
-          'Origin': 'https://blogapi.chengfen.online'
-        },
-        // 路径重写，确保正确转发
-        pathRewrite: {
-          '^/admin/api': '/admin/api'
-        }
-      }
-    }
   },
   outputDir: 'dist',
   publicPath: '/',
